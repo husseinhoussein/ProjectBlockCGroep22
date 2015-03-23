750ee8e13fdbae6b105aaaa5a9656c02c6451fa1
husseinhoussein <houssein.hussein@gmail.com>
2015-03-23 09:58:08 +0100

    dbcontroller

---
+++ Project The Greenery/TheGreenery/TheGreenery/DBcontrollers/DatabaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace TheGreenery.DBcontrollers
+{
+    public abstract class DatabaseController
+    {
+        protected MySqlConnection conn;
+
+        public DatabaseController()
+        {
+            //Vul hier de juiste gegevens in!!
+            conn = new MySqlConnection("Server=localhost;Database=lanparty;Uid=root;Pwd=root;");
+        }
+    }
+
+    }
+}
