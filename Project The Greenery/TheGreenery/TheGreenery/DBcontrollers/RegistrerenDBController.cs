c40f61bfb4538211bbf8fedac234ded47c6232ad
husseinhoussein <houssein.hussein@gmail.com>
2015-03-30 13:48:13 +0200

    \(OoO)/!!!!!!!!!
    booya

--- Project The Greenery/TheGreenery/TheGreenery/DBcontrollers/RegistrerenDBController.cs
+++ Project The Greenery/TheGreenery/TheGreenery/DBcontrollers/RegistrerenDBController.cs
@@ -22,48 +22,31 @@ namespace TheGreenery.DBcontrollers
             MySqlTransaction trans = null;
             conn.Open();
             trans = conn.BeginTransaction();
-            
+
             try
             {
-
-
-
-                
-                string insertString = @"insert into Klant ( voorletters, tussenvoegsel, achternaam, adres, postcode, woonplaats, 
-                                                    values (?voorletters, ?tussenvoegsel, ?achternaam, ?adres, ?postcode, ?woonplaats,
-                                                             ?telefoonnr, ?mail, ?wachtwoord, ?wachtwoord_herhalen);";
-//values (?voorletters, ?tussenvoegsel, ?achternaam, ?adres, ?postcode, ?woonplaats,  ?telefoonnr, ?mail, ?wachtwoord, ?wachtwoord_herhalen);";
-                                                
+                string insertString = @"
+                    insert into Klant 
+                        (voorletters, tussenvoegsel, achternaam, 
+                         adres, postcode, woonplaats, telefoonnr, 
+                         mail,  wachtwoord, wachtwoord_herhalen) 
+                        values 
+                        (@voorletters, @tussenvoegsel, @achternaam, 
+                         @adres, @postcode, @woonplaats, @telefoonnr, 
+                         @mail, @wachtwoord, @wachtwoord_herhalen);
+                ";
 
                 MySqlCommand cmd = new MySqlCommand(insertString, conn);
-              //cmd.CommandText = "insert into Klant(voorletters, tussenvoegsel, achternaam, adres, postcode, woonplaats, values(@voorletters, @tussenvoegsel, @achternaam, @adres, @postcode, @woonplaats,  @telefoonnr, @mail, @wachtwoord, @wachtwoord_herhalen);";
-              // cmd.Parameters.AddWithValue("?voorletters", MySqlDbType.VarChar).Value = klant.voorletters;
-                MySqlParameter voorlettersParam = new MySqlParameter("?voorletters", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?tussenvoegsel", MySqlDbType.VarChar).Value = klant.tussenvoegsel;
-                MySqlParameter tussenvoegselParam = new MySqlParameter("?tussenvoegsel", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?achternaam", MySqlDbType.VarChar).Value = klant.achternaam;
-                MySqlParameter achternaamParam = new MySqlParameter("?achternaam", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?adres", MySqlDbType.VarChar).Value = klant.adres;
-                MySqlParameter adresParam = new MySqlParameter("?adres", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?postcode", MySqlDbType.VarChar).Value = klant.postcode;
-                MySqlParameter postcodeParam = new MySqlParameter("?postcode", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?woonplaats", MySqlDbType.VarChar).Value = klant.woonplaats;
-                MySqlParameter woonplaatsParam = new MySqlParameter("?woonplaats", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?telefoonnr", MySqlDbType.VarChar).Value = klant.telefoonnr;
-                MySqlParameter telefoonnrParam = new MySqlParameter("?telefoonnr", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?mail", MySqlDbType.VarChar).Value = klant.mail;
-                MySqlParameter mailParam = new MySqlParameter("?mail", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?wachtwoord", MySqlDbType.VarChar).Value = klant.wachtwoord;
-                MySqlParameter wachtwoordParam = new MySqlParameter("?wachtwoord", MySqlDbType.VarChar);
-              //  cmd.Parameters.AddWithValue("?wachtwoord_herhalen", MySqlDbType.VarChar).Value = klant.wachtwoord_herhalen;
-                MySqlParameter wachtwoord_herhalenParam = new MySqlParameter("?wachtwoord_herhalen)", MySqlDbType.VarChar);
-                
-
-
-                
-
-
-               // Klant klant = new Klant();
+                MySqlParameter voorlettersParam = new MySqlParameter("@voorletters", MySqlDbType.VarChar);
+                MySqlParameter tussenvoegselParam = new MySqlParameter("@tussenvoegsel", MySqlDbType.VarChar);
+                MySqlParameter achternaamParam = new MySqlParameter("@achternaam", MySqlDbType.VarChar);
+                MySqlParameter adresParam = new MySqlParameter("@adres", MySqlDbType.VarChar);
+                MySqlParameter postcodeParam = new MySqlParameter("@postcode", MySqlDbType.VarChar);
+                MySqlParameter woonplaatsParam = new MySqlParameter("@woonplaats", MySqlDbType.VarChar);
+                MySqlParameter telefoonnrParam = new MySqlParameter("@telefoonnr", MySqlDbType.VarChar);
+                MySqlParameter mailParam = new MySqlParameter("@mail", MySqlDbType.VarChar);
+                MySqlParameter wachtwoordParam = new MySqlParameter("@wachtwoord", MySqlDbType.VarChar);
+                MySqlParameter wachtwoord_herhalenParam = new MySqlParameter("@wachtwoord_herhalen", MySqlDbType.VarChar);
 
                 voorlettersParam.Value = klant.voorletters;
                 tussenvoegselParam.Value = klant.tussenvoegsel;
@@ -75,7 +58,6 @@ namespace TheGreenery.DBcontrollers
                 mailParam.Value = klant.mail;
                 wachtwoordParam.Value = klant.wachtwoord;
                 wachtwoord_herhalenParam.Value = klant.wachtwoord_herhalen;
-
 
                 cmd.Parameters.Add(voorlettersParam);
                 cmd.Parameters.Add(tussenvoegselParam);
@@ -92,12 +74,10 @@ namespace TheGreenery.DBcontrollers
                 cmd.ExecuteNonQuery();
                 trans.Commit();
             }
-
-
             catch (Exception e)
             {
                 trans.Rollback();
-                Console.Write("Klant niet toegevoegd: " + e);
+                throw new Exception("Klant niet toegevoegd: " + e);
             }
             finally
             {
@@ -107,6 +87,6 @@ namespace TheGreenery.DBcontrollers
 
 
 
-        
+
     }
 }
