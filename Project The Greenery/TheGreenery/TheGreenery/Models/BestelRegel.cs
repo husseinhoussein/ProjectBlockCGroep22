92b71385108ba15944e2e0bc98159468d257e558
mlijpema <myleneij@xs4all.nl>
2015-04-08 14:08:07 +0200

    bestelregel model

---
+++ Project The Greenery/TheGreenery/TheGreenery/Models/BestelRegel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheGreenery.Models
+{
+    public class BestelRegel : Controller
+    {
+        public int regelnr;
+
+        public int bestellingnr;
+
+        public int productnr;
+
+        public int aantalEenheden;
+
+        public double regelPrijs;
+
+    }
+}
